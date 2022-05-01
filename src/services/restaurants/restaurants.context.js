7d3844cdb57b20d4079c32dad94fe460ecf94cc6
xNoxious <11569981+xNoxious@users.noreply.github.com>
2022-05-02 00:11:52 +0300

    Add search bar and location context

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,4 +1,5 @@
-import React, { useState, createContext, useEffect, useMemo } from 'react';
+import React, { useState, createContext, useEffect, useContext } from 'react';
+import { LocationContext } from '../location/location.context';
 import { restaurantsRequest, restaurantsTransform } from './restaurants.service';
 
 export const RestaurantsContext = createContext();
@@ -7,11 +8,14 @@ export const RestaurantsContextProvider = ({ children }) => {
     const [restaurants, setRestaurants] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const { location } = useContext(LocationContext);
 
-    const retrieveRestaurants = () => {
+    const retrieveRestaurants = (loc) => {
         setIsLoading(true);
+        setRestaurants([]);
+
         setTimeout(() => {
-            restaurantsRequest()
+            restaurantsRequest(loc)
                 .then(restaurantsTransform)
                 .then((results) => {
                     setIsLoading(false);
@@ -25,8 +29,11 @@ export const RestaurantsContextProvider = ({ children }) => {
     };
 
     useEffect(() => {
-        retrieveRestaurants();
-    }, []);
+        if (location) {
+            const locationString = `${location.lat},${location.lng}`;
+            retrieveRestaurants(locationString);
+        }
+    }, [location]);
 
     const value = {
         restaurants,
